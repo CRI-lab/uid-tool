e9af025dff6a4557cb9213278926cdd9be6ce722
Skyler Kimura <skyler8@hawaii.edu>
2023-09-18 13:52:29 -1000

    added poetry as dependency manager

--- tailwind.sh
+++ tailwind.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-source .venv/bin/activate
-tailwindcss -i app/static/src/main.css -o app/static/dist/main.css --watch &
-flask run --debug
+
+npx tailwind -i ./app/static/src/main.css -o ./app/static/dist/main.css
